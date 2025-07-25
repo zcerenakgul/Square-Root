3d152a744be7372d3f8976d464501537078f0ed2
hpome <hpome@CEREN>
2025-07-26 01:35:57 +0300

    Proje dosyası ekle.

---
+++ Square Root/Kaynak.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+	int squareRoot;
+	float number;
+	cout << "enter area of the square: " << endl;
+	cin >> squareRoot;
+	number = sqrt(squareRoot);
+	cout << "one sside of the square is : " << number << endl;
+	return 0;
+
+}
